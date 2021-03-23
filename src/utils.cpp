cba2e932a43062e89cd98457045b446cd021e1b8
Ramiro Basile <ramirobasile1@gmail.com>
2021-03-23 00:08:05 -0300

    Fix parser parsing float values as integer

--- src/utils.cpp
+++ src/utils.cpp
@@ -28,7 +28,7 @@ std::vector<fr::Punch> getPunches(toml::array unparsed) {
 	std::vector<fr::Punch> punches;
 
 	for (int i = 0; i < unparsed.size(); ++i) {
-		toml::table table = *unparsed.get_as<toml::table>(i);
+		toml::table table = *unparsed[i].as<toml::table>();
 		fr::Punch punch = parsePunch(table);
 		punches.push_back(punch);
 	}
@@ -42,9 +42,9 @@ fr::Punch parsePunch(toml::table punch) {
 	bool is_body = punch["is_body"].value_or(false);
 	int damage = punch["damage"].value_or(0);
 	int body_hp_cost = punch["body_hp_cost"].value_or(0);
-	float hitbox_begin = punch["hitbox_begin"].value_or(0);
-	float hitbox_end = punch["hitbox_end"].value_or(0);
-	float recovery_end = punch["recovery_end"].value_or(0);
+	float hitbox_begin = punch["hitbox_begin"].value_or(0.0f);
+	float hitbox_end = punch["hitbox_end"].value_or(0.0f);
+	float recovery_end = punch["recovery_end"].value_or(0.0f);
 	sf::IntRect hitbox(punch["hitbox"][0].value_or(0),
 			punch["hitbox"][1].value_or(0), punch["hitbox"][2].value_or(0),
 			punch["hitbox"][3].value_or(0));
